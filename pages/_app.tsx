c02604419ec9497683b2c0092f7be30e5e71321c
MyeongheeJung <dobby.jmh@gmail.com>
2023-04-15 14:04:19 +0900

    폴더구조 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import Layout from "../src/components/commons/layouts";
-import { globalStyles } from "../src/styles/globalStyles";
+import { globalStyles } from "../src/commons/styles/globalStyles";
 import { Global } from "@emotion/react";
 
 export default function App({ Component, pageProps }: AppProps) {
