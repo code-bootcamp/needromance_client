a4f018b3518ced2523dd7576fa53c600d5cc7d4e
RumbleBi <olzlel2000@gmail.com>
2023-05-02 22:29:49 +0900

    chatGPT 페이지 동적 라우팅 구현

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   compiler: { emotion: true },
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://need-romance.site/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       destination: "https://need-romance.site/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
