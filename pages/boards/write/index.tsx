581875f045ac6f4bfbf11ab26cb3ccee48a04c48
MyeongheeJung <dobby.jmh@gmail.com>
2023-05-04 13:22:07 +0900

    권한분기

--- pages/boards/write/index.tsx
+++ pages/boards/write/index.tsx
@@ -1,6 +1,10 @@
+import useAuth from "../../../src/components/commons/hooks/customs/useAuth";
 import CommunityWrite from "../../../src/components/units/boards/write/CommunityWrite";
 
 export default function CommunityWritePage() {
+  // 권한분기
+  useAuth();
+
   return (
     <div>
       <CommunityWrite isEdit={false} />
