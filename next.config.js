edad961eb49d4fd098a66bfdeaa44b1cf3cbc08e
RumbleBi <olzlel2000@gmail.com>
2023-05-05 14:44:51 +0900

    next.config.js cors에러 수정

--- next.config.js
+++ next.config.js
@@ -8,6 +8,28 @@ const nextConfig = {
   },
   reactStrictMode: true,
   compiler: { emotion: true },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://need-romance.site",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PATCH, DELETE, OPTIONS",
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
