e71167c22456ddad1288449e05f4197914776f40
RumbleBi <olzlel2000@gmail.com>
2023-05-09 15:12:19 +0900

    footer UI 변경

--- src/components/commons/layouts/footer/LayoutFooter.tsx
+++ src/components/commons/layouts/footer/LayoutFooter.tsx
@@ -1,14 +1,13 @@
+import { GithubFilled } from "@ant-design/icons";
 import styled from "@emotion/styled";
-import { FaGithub } from "react-icons/fa";
 
 const Wrapper = styled.footer`
   width: 100%;
-  margin-top: 20px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   font-size: 0.8rem;
-  padding: 20px 4px;
+  padding: 20px 5px;
   font-family: var(--eng-font);
 `;
 
@@ -16,7 +15,6 @@ const Info = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin-bottom: 20px;
 `;
 
 const InfoLeft = styled.div`
@@ -25,19 +23,17 @@ const InfoLeft = styled.div`
 
 const InfoRight = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: right;
   font-size: 1.2rem;
 `;
 
-const GitBtn = styled.div`
+const GitBtn = styled.a`
   cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  justify-content: right;
-  span {
-    margin-left: 2px;
-    line-height: 1.2;
+  > span {
+    font-weight: bold;
+    font-size: var(--font-size-sm);
+    margin-left: 5px;
   }
 `;
 
@@ -48,29 +44,33 @@ export default function LayoutFooter() {
         <InfoLeft>
           (주)로맨스가 필요해
           <br />
-          대표 : 2조 조신정김
+          대표 : 조신정김
           <br />
-          고객센터 : 2222 - 1004
+          고객센터 : 2222 - 1004 | (평일 09:00~18:00/점심시간 13:00~14:00/ 주말
+          및 공휴일 휴무)
           <br />
-          (평일 09:00~18:00/점심시간 13:00~14:00/ 주말 및 공휴일 휴무)
-          <br />
-          사업자등록번호 : 000-00-00000 <br />
+          사업자등록번호 : 000-00-00000 | Copyright© Needromance(주) All rights
+          reserved.
         </InfoLeft>
         <InfoRight>
           <GitBtn
-            onClick={() =>
-              window.open(
-                "https://github.com/code-bootcamp/pbp1_team02_client",
-                "_blank"
-              )
-            }
+            href="https://github.com/code-bootcamp/pbp1_team02_client"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <FaGithub />
-            <span>Github</span>
+            <GithubFilled />
+            <span>프론트 저장소</span>
+          </GitBtn>
+          <GitBtn
+            href="https://google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubFilled />
+            <span>백엔드 저장소</span>
           </GitBtn>
         </InfoRight>
       </Info>
-      Copyright© INeedRomance(주) All rights reserved.
     </Wrapper>
   );
 }
